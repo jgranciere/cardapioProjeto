88e7cc2cc105194641e3cc2c0764612d8f268185
thiago0-08 <housezx210@gmail.com>
2025-05-28 22:13:48 -0300

    add funcao para enviar os produtos para o carrinho

--- src/Components/pages/Productdetails.jsx
+++ src/Components/pages/Productdetails.jsx
@@ -1,68 +1,95 @@
 import { useLocation, useParams } from 'react-router-dom'
+import { useState } from 'react';
 import './ProductDetails.css'
 import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import BarraInferior from '../BarraInferior/barraInferior'
 import Listaprodutos from '../ListaProdutos/Listaprodutos'
+import { useCarrinho } from '../../context/CarrinhoContext';
 import React, { useContext } from 'react';
 import { ProdutosContext } from '../../context/ProdutosContext';
 
 
 const ProductDetails = () => {
-  const { produtos } = useContext(ProdutosContext);
-  console.log(produtos);
-  const { state } = useLocation();  
-  const { id } = useParams()
-  const navigate = useNavigate();
-  
-  const produtoSelecionado = state?.produto || state?.bebida;
+    const { produtos } = useContext(ProdutosContext);
+    console.log(produtos);
+    const { state } = useLocation();
+    const { id } = useParams()
+    const navigate = useNavigate();
+    const { adicionarAoCarrinho } = useCarrinho();
 
 
-  if (!produtoSelecionado){
-    return <p>Produto não encontrado para o ID: {id}</p>
-  }
+    const produtoSelecionado = state?.produto || state?.bebida;
 
-  const produtosRecomendados = produtos.filter(p => p.id !== produtoSelecionado.id); 
 
-  return (
-    <div className='produto-detail-container'>
-        <div className='produto-detail-header'>
-            <div className='produto-detail-img'>
-                <FontAwesomeIcon icon={faArrowLeft} className='icone' onClick={() => navigate ("/")}/>
-                <img src={produtoSelecionado.imagemUrl} alt={`Imagem do ${produtoSelecionado.nome}`} />
-            </div>
+    if (!produtoSelecionado) {
+        return <p>Produto não encontrado para o ID: {id}</p>
+    }
 
-            <div className='produto-detail-info'>
-                <h1>{produtoSelecionado.nome}</h1>
-                <p>{produtoSelecionado.descricao}</p>
-                <span>R$ {produtoSelecionado.preco}</span>
-            </div>
+    const [mensagem, setMensagem] = useState('');
 
-            <div className='produto-more-separete'>
-                <div className='produto-separate'>
-                    <p>Escolha mais itens !</p>
+    const handleAdicionarCarrinho = () => {
+        const produto = {
+            id: produtoSelecionado.id,
+            nome: produtoSelecionado.nome,
+            preco: produtoSelecionado.preco,
+            imagemUrl: produtoSelecionado.imagemUrl
+        };
+        adicionarAoCarrinho(produto);
+        setMensagem('✅ Produto adicionado ao carrinho!');
+
+        // Limpa a mensagem depois de 3 segundos
+        setTimeout(() => setMensagem(''), 3000);
+    };
+
+
+    const produtosRecomendados = produtos.filter(p => p.id !== produtoSelecionado.id);
+
+    return (
+        <div className='produto-detail-container'>
+            <div className='produto-detail-header'>
+                <div className='produto-detail-img'>
+                    <FontAwesomeIcon icon={faArrowLeft} className='icone' onClick={() => navigate("/")} />
+                    <img src={produtoSelecionado.imagemUrl} alt={`Imagem do ${produtoSelecionado.nome}`} />
                 </div>
-            </div>
 
-            <div className='produto-more-list-details'>
-                <div className='produto-more-list-cards'>
-                    {produtosRecomendados.map(produto => (
-                        <div key={produto.id} className='produto-recomended-card'>
-                            <div className='produto-recomended-name'>
-                                <p>{produto.nome}</p>
-                                <p>R$ {produto.preco}</p>
+                <div className='produto-detail-info'>
+                    <h1>{produtoSelecionado.nome}</h1>
+                    <p>{produtoSelecionado.descricao}</p>
+                    <span>R$ {produtoSelecionado.preco}</span>
+                    <button onClick={handleAdicionarCarrinho} className="botao-adicionar-carrinho">
+                        Adicionar ao Carrinho
+                    </button>
+                    {mensagem && <div className="mensagem-carrinho">{mensagem}</div>}
+
+
+                </div>
+
+                <div className='produto-more-separete'>
+                    <div className='produto-separate'>
+                        <p>Escolha mais itens !</p>
+                    </div>
+                </div>
+
+                <div className='produto-more-list-details'>
+                    <div className='produto-more-list-cards'>
+                        {produtosRecomendados.map(produto => (
+                            <div key={produto.id} className='produto-recomended-card'>
+                                <div className='produto-recomended-name'>
+                                    <p>{produto.nome}</p>
+                                    <p>R$ {produto.preco}</p>
+                                </div>
+
+                                <img src={produto.imagemUrl} alt={`Imagem do ${produto.nome}`} />
                             </div>
-                            
-                            <img src={produto.imagemUrl} alt={`Imagem do ${produto.nome}`} />
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
+            <BarraInferior />
         </div>
-        <BarraInferior/>
-    </div>
-  )
+    )
 }
 
 export default ProductDetails
